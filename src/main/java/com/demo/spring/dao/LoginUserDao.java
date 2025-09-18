7f839e578076c959baff25b78f71031ddc69a5a2
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-09-18 10:45:27 +0530

    project initialization

---
+++ src/main/java/com/demo/spring/dao/LoginUserDao.java
@@ -0,0 +1,7 @@
+package com.demo.spring.dao;
+import org.springframework.data.repository.CrudRepository;
+
+import com.demo.spring.entites.Login;
+public interface LoginUserDao  extends CrudRepository<Login, Integer>{
+
+}
---
+++ src/main/java/com/demo/spring/service/UserService.java
@@ -0,0 +1,35 @@
+package com.demo.spring.service;
+import com.demo.spring.entites.Login;
+import com.demo.spring.entites.Signup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.spring.dao.LoginDao;
+import com.demo.spring.dao.LoginUserDao;
+import com.demo.spring.dao.SignupDao;
+@Service
+public class UserService {
+	@Autowired
+	private LoginDao logindao;
+	@Autowired
+	private SignupDao signupdao;
+	@Autowired
+	private LoginUserDao loginuserdao;
+	public void signup(Signup signup) {
+		// TODO Auto-generated method stub
+		Login login = new Login();
+		signupdao.save(signup);
+		login.setId(signup.getId());
+		login.setUsername(signup.getUsername());
+		login.setPassword(signup.getPassword());
+		logindao.save(login);
+		
+	}
+	
+	public Login getuserbyid(int id) {
+		// TODO Auto-generated method stub
+		return loginuserdao.findById(id).get();
+		
+	}
+
+}
